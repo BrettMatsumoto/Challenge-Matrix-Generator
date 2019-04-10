ba1d347b8296a8c84edbef8172ae34b9b154b86e
Brett <brett_matsumoto@yahoo.com>
2019-04-10 09:11:10 -1000

    reformatted code

--- matrix-generator.js
+++ matrix-generator.js
@@ -3,16 +3,21 @@
 function matrixGenerator(rows, columns) {
   // do work here
   let matrix = [];
+
   for (let i = 0; i < rows; i++) {
     let columnArr = [];
+
     for (let j = 0; j < columns; j++) {
       columnArr.push(Math.floor(Math.random * 100))
       columnArr.sort();
     }
+
     matrix.push(columnArr);
   }
+
   return matrix
 }
+
 console.log(matrixGenerator())
 
 
